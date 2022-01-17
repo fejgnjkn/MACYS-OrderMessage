049b50083903047dc050000851288931979977cb
Nawaz <vmnawaz@gmail.com>
2022-01-17 20:36:26 +0530

    changes made to test case

--- src/main/java/com/zensar/controller/OrderController.java
+++ src/main/java/com/zensar/controller/OrderController.java
@@ -6,11 +6,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.zensar.dto.OrderMessageRequest;
 import com.zensar.services.OrderService;
 
 
+@RestController
 public class OrderController {
 	
 	@Autowired
--- src/test/java/com/zensar/controller/OrderControllerTest.java
+++ src/test/java/com/zensar/controller/OrderControllerTest.java
@@ -1,18 +1,32 @@
 package com.zensar.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.zensar.dto.OrderMessageRequest;
 import com.zensar.services.OrderService;
 
+import ch.qos.logback.core.status.Status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class OrderControllerTest {
 
 //	@InjectMocks
@@ -32,9 +46,15 @@ public class OrderControllerTest {
 //
 //	}
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	final void testUpdateOrder() {
+	final void testUpdateOrder() throws Exception {
 //		when(orderService.updateOderStatus(0, ordReq)).thenReturn(true);
+		String updateStatus = "{\"orderStatus\":\"open\"}";
 		assertEquals(true, true);
+		mockMvc.perform(MockMvcRequestBuilders.put("/7").contentType(MediaType.APPLICATION_JSON)
+				.content(updateStatus).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
 	}
 }
